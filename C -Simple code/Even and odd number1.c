1ae01f7db49c142b551d3178833460d0f713c831
Md Toriqul islam <31108894+Toriqul35@users.noreply.github.com>
2018-03-06 04:52:51 -0600

    no

---
+++ C -Simple code/Even and odd number1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+   int T,I,N;
+   scanf("%d",&T);
+   for(I=1;I<=T;I++)
+    {
+        scanf("%d",&N);
+        if ( N%2 == 0)
+   {
+       printf("even\n");
+   }
+   else
+    {
+        printf("odd\n");
+
+   }
+
+   }
+   return 0;
+
+
+}
